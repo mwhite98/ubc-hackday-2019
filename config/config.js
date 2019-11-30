c56a952de83ea450bcee8ee3b5d01939348f1b9d
Jackson Yuan <jackson.yuan@move.com>
2019-11-30 13:07:43 -0800

    added sample data & set up backend config

---
+++ config/config.js
@@ -0,0 +1,26 @@
+// require('dotenv').config();
+
+
+const env = process.env.NODE_ENV; // 'dev' or 'test'
+const pool = require('./config/db')
+
+const dev = {
+    app: {
+        port: 3000,
+        secret: 'secret',
+    },
+    db: {
+        host: 'localhost',
+        port: 80,
+        name: 'lhd',
+        user: 'root',
+        pass: '',
+    }
+};
+
+// const config = {
+//     dev,
+// };
+
+
+module.exports = dev;
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const config = require("./config/config");
+const port = config.app.port;
+const pool = require("./config/db");
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+  	res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+app.get('/get_event' , (req, res) => {
+
+});
+
+// get_threads
+// get_thread
+
